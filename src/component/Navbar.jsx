bcea7b0afd5105715be0bf74328af26f30e4a7fe
JP <pumjirach@gmail.com>
2025-02-07 14:01:17 +0700

    update playlist

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { FaHome, FaHeart, FaMusic, FaUserCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  return (
+    <nav className="sticky top-0 bg-[#A35D4E] text-white p-4 flex items-center justify-between border-b-4 border-white z-50">
+      <div className="flex items-center space-x-2">
+        <img src=".\src\assets\logo.png" alt="Event Music Selector" className="h-14" />
+      </div>
+      <div className="absolute left-1/2 transform -translate-x-1/2 mt-4 flex space-x-6 text-xl">
+      <NavItem onClick={() => navigate("/")} icon={<FaHome />} label="Home" />
+        <NavItem onClick={() => navigate("/playlist")} icon={<FaMusic />} label="Playlist" />
+        <NavItem  icon={<FaHeart />} label="Favorite" />
+      </div>
+
+      <FaUserCircle className="text-3xl" />
+    </nav>
+  );
+};
+
+const NavItem = ({ onClick, icon, label }) => (
+  <div onClick={onClick} className="flex items-center space-x-1 cursor-pointer hover:text-yellow-300">
+    {icon}
+    <span>{label}</span>
+  </div>
+);
+
+export default Navbar;
